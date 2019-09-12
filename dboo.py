87cc76cd7cccd8e578f04460ef244ff4fd0ede48
isowang <isowang@126.com>
2019-09-12 18:09:16 +0800

    修改完成时间

--- dboo.py
+++ dboo.py
@@ -139,7 +139,7 @@ def gettimedata():
     conn = sqlite3.connect('C:/Users/fengy/OneDrive/文档/tmss.db')
     c = conn.cursor()
     cursor = c.execute(
-        "select ftime,sum(times) from task where isfinish =1 and isabandon=0 group by ftime")
+        "select strftime('%Y-%m-%d',ftime),sum(times) from task where isfinish =1 and isabandon=0 group by strftime('%Y-%m-%d',ftime)")
     result = []
     for row in cursor:
         result.append(row)
@@ -151,7 +151,7 @@ def gettimedata():
 def finishtask(task_id, task_numbers):
     conn = sqlite3.connect('C:/Users/fengy/OneDrive/文档/tmss.db')
     # 格式化成2016-03-20 11:45:39形式
-    etime = time.strftime("%Y-%m-%d %M:%H:%S", time.localtime())
+    etime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     c = conn.cursor()
     c.execute('''update task set ftime=? ,isfinish=1 ,times=? where task_id =? ''', [
               etime, task_numbers, task_id])
