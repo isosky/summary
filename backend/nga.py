98198d77b4e3386247cefc914023d22ddd490e3c
isowang <isowang@126.com>
2021-01-09 16:29:04 +0800

    nga 优化算法

--- backend/nga.py
+++ backend/nga.py
@@ -102,8 +102,7 @@ class database(object):
         sql = 'insert into nga_attach (post_id,poster_id,replys,attach_url) values (%s,%s,%s,%s) on duplicate key update replys=VALUES(replys)'
         for i in data:
             try:
-                print(i)
-                self.cursor.executemany(sql, i)
+                self.cursor.execute(sql, i)
                 self.db.commit()
             except Exception as identifier:
                 print(identifier)
@@ -287,6 +286,7 @@ class costThread(threading.Thread):
                 print(self.name, '还有', self.task_queue.qsize(), '个任务')
                 getonepage(t[0], t[1], t[2])
                 time.sleep(10)
+                continue
             print(self.name, "等待休眠")
             time.sleep(60)
 
@@ -372,9 +372,9 @@ if __name__ == "__main__":
     t = [postThread(99, 'post', task_queue)]
     t.append(imgpostThread(90, 'get img', img_queue))
 
-    # print("准备启动：5 个|抓图|线程")
-    # for i in range(31, 36):
-    #     t.append(imggetThread(i, "Thread" + str(i), img_queue))
+    print("准备启动：5 个|抓图|线程")
+    for i in range(31, 36):
+        t.append(imggetThread(i, "Thread" + str(i), img_queue))
 
     print("准备启动：10 个|抓帖子|线程")
     for i in range(10):
