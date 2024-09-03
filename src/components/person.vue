3f5dd2b32b602c5a23408d0fa6bed917da527b5b
isowang@126.com <isowang@126.com>
2024-09-03 21:20:22 +0800

    add:人员添加部门

--- src/components/person.vue
+++ src/components/person.vue
@@ -11,34 +11,31 @@
           style="width: 140px"
         >
           <el-option
-            v-for="item in company_selector"
+            v-for="item in company_option"
             :key="item.value"
             :label="item.label"
             :value="item.value"
           >
           </el-option>
         </el-select>
-        <el-input
-          v-model="person_name"
-          style="width: 120px"
-          placeholder="请输入姓名"
-        ></el-input>
-        <el-button
-          @click="addperson"
-          icon="el-icon-check"
-          circle
-          type="success"
-        ></el-button>
-        <el-button
-          @click="addperson"
-          icon="el-icon-search"
-          circle
-          type="success"
-        ></el-button>
-      </el-row>
-      <el-row :span="6">
         <el-select
-          v-model="person_post"
+          v-model="department"
+          filterable
+          allow-create
+          default-first-option
+          placeholder="请选择部门"
+          style="width: 140px"
+        >
+          <el-option
+            v-for="item in department_option"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          >
+          </el-option>
+        </el-select>
+        <el-select
+          v-model="post"
           filterable
           allow-create
           default-first-option
@@ -46,22 +43,43 @@
           style="width: 140px"
         >
           <el-option
-            v-for="item in person_post_selector"
+            v-for="item in post_option"
             :key="item.value"
             :label="item.label"
             :value="item.value"
           >
           </el-option>
         </el-select>
-        <el-checkbox v-model="person_forceadd">强制添加 </el-checkbox>
+
       </el-row>
       <el-row :span="6">
+        <el-input
+          v-model="person_name"
+          style="width: 120px"
+          placeholder="请输入姓名"
+        ></el-input>
+        <el-checkbox v-model="person_forceadd">强制添加 </el-checkbox>
+        <el-button
+          @click="addperson"
+          icon="el-icon-check"
+          type="success"
+        ></el-button>
+        <el-button
+          @click="addperson"
+          icon="el-icon-search"
+          type="success"
+        ></el-button>
+      </el-row>
+      <el-row :span="6">
+        <!-- TODO 表格排序 -->
         <el-table :data="persondata" height="800" style="width: 100%">
           <el-table-column prop="company" label="单位" width="80">
           </el-table-column>
-          <el-table-column prop="person_name" label="名称" width="120">
+          <el-table-column prop="department" label="部门" width="80">
           </el-table-column>
-          <el-table-column prop="person_post" label="职位" width="80">
+          <el-table-column prop="post" label="职位" width="80">
+          </el-table-column>
+          <el-table-column prop="person_name" label="名称" width="120">
           </el-table-column>
           <el-table-column label="操作" width="120">
             <template slot-scope="scope">
@@ -92,25 +110,28 @@ export default {
   data() {
     return {
       persondata: [],
-      company_selector: [],
-      company: "",
       person_name: "",
-      person_post: "",
-      person_post_selector: [],
+      company: "",
+      company_option: [],
+      department:"",
+      department_option:"",
+      post: "",
+      post_option: [],
       person_forceadd: false,
     };
   },
   mounted: function () {
-    this.getcompany();
+    this.getoptions();
     this.getperson();
   },
   methods: {
     // person
-    getcompany: function () {
-      axios.get("/getcompany").then((response) => {
+    getoptions: function () {
+      axios.get("/getpersonoptions").then((response) => {
         // console.log(response);
-        this.company_selector = response.data.company_selector;
-        this.person_post_selector = response.data.person_post_selector;
+        this.company_option = response.data.company_options;
+        this.department_option = response.data.department_options;
+        this.post_option = response.data.post_options;
       });
     },
     getperson: function () {
@@ -124,17 +145,19 @@ export default {
         axios
           .post("/addperson", {
             company: this.company,
+            department: this.department,
             person_name: this.person_name,
-            person_post: this.person_post,
+            post: this.post,
             force: this.person_forceadd,
           })
           .then((response) => {
             if (response.data.msg == true) {
               this.company = "";
+              this.department = "";
               this.person_name = "";
-              this.person_post = "";
+              this.post = "";
               this.getperson();
-              this.getcompany();
+              this.getoptions();
             } else {
               this.$message.error("有重名的，请确认是否强制添加");
             }
@@ -150,7 +173,7 @@ export default {
         })
         .then((response) => {
           this.getperson();
-          this.getcompany();
+          this.getoptions();
         });
     },
   },
